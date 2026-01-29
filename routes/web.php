e76bec13adbadab99fa66b06c34a0cd816852724
sgmt <s.shigematsu17@gmail.com>
2026-01-29 23:34:40 +0900

    chore: 認証機能に関連する新しいコンポーネントとルートを追加

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,35 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-// Welcome route
-Route::get('/', fn () => view('welcome'));
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Book management routes
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
-Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Edit, Update, Delete routes for books management(後ほどミドルウェアを適用)
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::patch('/books/{book}', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+    // Book management routes
+    Route::get('/books', [BookController::class, 'index'])->name('books.index');
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
+    Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
+
+    // Edit, Update, Delete routes for books management(後ほどミドルウェアを適用)
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::patch('/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+    Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+
+});
+
+require __DIR__.'/auth.php';
